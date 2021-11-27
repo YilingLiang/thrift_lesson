0b8515fe06263d49bd9bc04947b86c2a9d7bc75f
yilingliang <15600992130@163.com>
2021-11-27 13:59:11 +0800

    add match server

--- thrift/match.thrift
+++ thrift/match.thrift
@@ -7,7 +7,19 @@ struct User {
 }
 
 service Match {
-    i32 add_user{1: User user, 2: string info},
 
-    i32 remove_user{1: User user, 2: string info },
+    /**
+     * user: 添加的用户信息
+     * info: 附加信息
+     * 在匹配池中添加一个名用户
+     */
+    i32 add_user(1: User user, 2: string info),
+
+    /**
+     * user: 删除的用户信息
+     * info: 附加信息
+     * 从匹配池中删除一名用户
+     */
+    i32 remove_user(1: User user, 2: string info),
 }
+
